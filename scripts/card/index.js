4e8ae1a4c79a9f9ba1052da29c26142772a0946c
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-02-17 09:08:05 -0300

    compress para deploy

--- scripts/card/index.js
+++ scripts/card/index.js
@@ -1,116 +1,8 @@
-class Card {
-  constructor(img,name,type,id, stats) {
-    this.img = img;
-    this.name = name;
-    this.type = type; 
-    this.id = id;
-    this.stats = stats
-   
-  }
-  
-  criarCard(){
-    
-
-    let nameType = [];
-    let estiloBackGroud = [];
-  
-    
-  for( let i = 0 ; i < this.type.length ; i++){
-
-    nameType.push(this.type[i].type.name) 
-
-  }
-
-  
-
-  
-  const passarStatsNoArray = (array) => {
-    array.push(this.stats)
-  }
-  passarStatsNoArray(arrayStats)
-  
-
-  const passarImgsNoArrray = (array) => {
-     
-    array.push( this.img)
-    
-  }
-  passarImgsNoArrray(arrayImgs)
-
-  const passarNameParaArray = (array) => { array.push( this.name)  }
-  
-    passarNameParaArray(arrayName)
-    
- 
-
-
-
-  const funcBack=()=>{
-    nameType.map( (i) => {
-      
-    
-    if (i == 'water' || i == 'ice' || i == 'flying') {
-    
-      estiloBackGroud.push( 'background-image:var(--backGroud-water-ice-flying);');
-    
-    
-    }
-    if (i == 'fire') {
-     estiloBackGroud.push( 'background-image:var(--backGroud-fire);')
-    
-    }
-    if (i == 'electric' || i == 'fairy') {
-    
-      estiloBackGroud.push('background-image:va(--backGroud-eletric-fairy);')
-    
-    }
-    if (i == 'grass' || i == 'dragon' || i == 'bug') {
-    
-      estiloBackGroud.push('background-image:var(--backGroud-gass-dragon-bug);')
-    
-    }
-    if (i == 'ghost' || i == 'rock') {
-    
-      estiloBackGroud.push('background-image:var(  --backGroud-ghost-rock);')
-    
-    }
-    if (i == 'dark' || i == 'psychic') {
-    
-      estiloBackGroud.push('background-image:var(   --backGroud-dark-psychic);')
-    
-    }
-    if (i == 'poison' || i == 'fighting') {
-    
-      estiloBackGroud.push('background-image:var(   --backGroud-poison-fighting);')
-    
-    }
-    if (i == 'normal') {
-    
-      estiloBackGroud.push('background-image:var(     --backGroud-normal);')
-    
-    }
-    if (i == 'ground' || i == 'stell') {
-    
-      estiloBackGroud.push('background-image:var(       --backGroud-ground-stell);')
-    
-    }
-    
-    
-    
-    })}
-    funcBack()
-    
-
-
- 
-    
-    
-   
-  return` 
+class Card{constructor(a,b,c,d,e){this.img=a,this.name=b,this.type=c,this.id=d,this.stats=e}criarCard(){let a=[],b=[];for(let b=0;b<this.type.length;b++)a.push(this.type[b].type.name);(a=>{a.push(this.stats)})(arrayStats);(a=>{a.push(this.img)})(arrayImgs);(a=>{a.push(this.name)})(arrayName);return(()=>{a.map(a=>{("water"==a||"ice"==a||"flying"==a)&&b.push("background-image:var(--backGroud-water-ice-flying);"),"fire"==a&&b.push("background-image:var(--backGroud-fire);"),("electric"==a||"fairy"==a)&&b.push("background-image:va(--backGroud-eletric-fairy);"),("grass"==a||"dragon"==a||"bug"==a)&&b.push("background-image:var(--backGroud-gass-dragon-bug);"),("ghost"==a||"rock"==a)&&b.push("background-image:var(  --backGroud-ghost-rock);"),("dark"==a||"psychic"==a)&&b.push("background-image:var(   --backGroud-dark-psychic);"),("poison"==a||"fighting"==a)&&b.push("background-image:var(   --backGroud-poison-fighting);"),"normal"==a&&b.push("background-image:var(     --backGroud-normal);"),("ground"==a||"stell"==a)&&b.push("background-image:var(       --backGroud-ground-stell);")})})(),` 
     
   <a class='linkCard' id='${this.id}' >
   <div class = 'test'>  </div>
-    <div style = '${ estiloBackGroud.map( (y) => { return y})}' class = 'divCard'> 
+    <div style = '${b.map(a=>a)}' class = 'divCard'> 
      
      <div  class = 'headerCard'> <span>${this.name}</span> <span>#${this.id} </span> </div> 
       
@@ -118,36 +10,14 @@ class Card {
       
       <div><img src= '${this.img}'> </div>
       
-      <div class ='divSpanType'>  ${ nameType.map( (x) => { return `<span class ='spanType'>${x}</span> `})} </div> 
+      <div class ='divSpanType'>  ${a.map(a=>`<span class ='spanType'>${a}</span> `)} </div> 
       </div> 
       
     </div> 
     
   </a>
 
-    `
-}
-
-  }
-  
- 
-  class MoreCardClass {
-
-    constructor(id, name, arrayStats,img){
-      
-      this.id = id;
-      this.name = name;
-      this.arrayStats = arrayStats;
-      this.img = img
-    }
-
-    moreInfor(){
-        
-      
-      console.log('more'); 
-      
-    
-      return `   
+    `}}class MoreCardClass{constructor(a,b,c,d){this.id=a,this.name=b,this.arrayStats=c,this.img=d}moreInfor(){return console.log("more"),`   
    
       <div class = 'container_conteudo'> 
  
@@ -178,18 +48,12 @@ class Card {
       <ul class = 'list_base_states'>
 
       <h1 class = 'base_states'> Base states</h1>
-       <li> <b>Hp</b> <p>${this.arrayStats[0].base_stat
-       }</p>  </li>
-       <li> <b>Attack</b> <p>${this.arrayStats[1].base_stat
-       }</p>  </li>
-       <li> <b>Defense</b> <p>${this.arrayStats[2].base_stat
-       }</p>  </li>
-       <li> <b>spacial-Attack </b> <p>${this.arrayStats[3].base_stat
-       }</p>  </li>
-       <li> <b>spacial-Defense </b> <p>${this.arrayStats[4].base_stat
-       }</p>  </li>
-      <li> <b>speed </b> <p>${this.arrayStats[5].base_stat
-      }</p>  </li>
+       <li> <b>Hp</b> <p>${this.arrayStats[0].base_stat}</p>  </li>
+       <li> <b>Attack</b> <p>${this.arrayStats[1].base_stat}</p>  </li>
+       <li> <b>Defense</b> <p>${this.arrayStats[2].base_stat}</p>  </li>
+       <li> <b>spacial-Attack </b> <p>${this.arrayStats[3].base_stat}</p>  </li>
+       <li> <b>spacial-Defense </b> <p>${this.arrayStats[4].base_stat}</p>  </li>
+      <li> <b>speed </b> <p>${this.arrayStats[5].base_stat}</p>  </li>
 
       </ul>
       
@@ -199,10 +63,4 @@ class Card {
 
 
     </div> 
-   `
-    }
-
-
-  }
-   
-   
+   `}}
